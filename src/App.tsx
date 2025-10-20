6b67b6136a760de272a3fd8fbff9be5fa2ed3594
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-10-20 18:40:38 +0800

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <h1 className="text-blue-500">HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
